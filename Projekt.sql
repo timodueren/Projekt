99a1b6952c8eec74c2e4f64dbb938b9af44c06d4
timodueren <45594195+timodueren@users.noreply.github.com>
2019-01-03 17:38:12 +0100

    Add files via upload

--- Projekt.sql
+++ Projekt.sql
@@ -1,11 +1,32 @@
 SHOW DATABASES;
 
-create schema Datenbank;
-USE Datenbank;
+
+create schema Eintrag;
+USE Eintrag;
+
+create table if not exists Kundendaten(
+  id int (100),
+  name varchar(255),
+  vorname varchar(255),
+  strasse varchar(255),
+  hausnummer varchar(10),
+  postleitzahl int(5),
+  loginname varchar(20),
+  loginpasswort varchar(20),
+  PRIMARY KEY (id)
+);
 
 
+create table Buchung(
+  kundeid int(100),
+  speisen int(1),
+  getraenke int(1),
+  unterhaltungen int(1),
+  extras int (1),
 
-
+  PRIMARY KEY (kundeid),
+  FOREIGN KEY (kundeid) references Kundendaten (id)
+);
 
 
 
@@ -13,4 +34,4 @@ USE Datenbank;
 show COLUMNS from Kundendaten;
 show columns from Buchung;
 
-drop schema Datenbank;
+drop schema Eintrag;
