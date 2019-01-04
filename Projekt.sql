cef6897b65c9b829d0ca535104afac88348cffc9
timodueren <45594195+timodueren@users.noreply.github.com>
2019-01-04 09:54:16 +0100

    Add files via upload

--- Projekt.sql
+++ Projekt.sql
@@ -1,7 +1,7 @@
 SHOW DATABASES;
 
 
-create schema Eintrag;
+create schema if not exists Eintrag;
 USE Eintrag;
 
 create table if not exists Kundendaten(
@@ -17,21 +17,66 @@ create table if not exists Kundendaten(
 );
 
 
-create table Buchung(
+create table if not exists Buchung(
+  Buchung int(1),
   kundeid int(100),
-  speisen int(1),
-  getraenke int(1),
-  unterhaltungen int(1),
-  extras int (1),
+  speisen boolean,
+  getraenke boolean,
+  unterhaltungen boolean,
+  extras boolean,
 
   PRIMARY KEY (kundeid),
+  PRIMARY KEY (Buchung),
   FOREIGN KEY (kundeid) references Kundendaten (id)
 );
 
+create table if not exists speisen(
+  Buchungsid int(1),
+
+  Hochzeitssuppe int(1),
+
+
+  PRIMARY KEY (Buchungsid),
+  Foreign Key (Buchungsid) references Buchung (Buchung)
+);
+
+create table if not exists getraenke(
+  Buchungsid int(1),
+
+  Hochzeitssuppe int(1),
+
+
+  PRIMARY KEY (Buchungsid),
+  Foreign Key (Buchungsid) references Buchung (Buchung)
+);
+
+create table if not exists unterhaltungen(
+  Buchungsid int(1),
+
+  Hochzeitssuppe int(1),
+
+
+  PRIMARY KEY (Buchungsid),
+  Foreign Key (Buchungsid) references Buchung (Buchung)
+);
+
+create table if not exists extras(
+  Buchungsid int(1),
+
+  Hochzeitssuppe int(1),
+
+
+  PRIMARY KEY (Buchungsid),
+  Foreign Key (Buchungsid) references Buchung (Buchung)
+);
 
 
 
 show COLUMNS from Kundendaten;
 show columns from Buchung;
+show columns from speisen;
+show columns from getraenke;
+show columns from unterhaltungen;
+show columns from extras;
 
-drop schema Eintrag;
+/* drop schema Eintrag;
